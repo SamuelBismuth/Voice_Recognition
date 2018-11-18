d9bb55d6d8666a16df08bbe076457ed8267470e4
yoshago <33255013+yoshago@users.noreply.github.com>
2018-11-18 19:18:29 +0200

    adding  some data to the dataset

--- Multinomial_logistic_regression/Main.py
+++ Multinomial_logistic_regression/Main.py
@@ -8,13 +8,15 @@ from prepare_wav import song_time, divide_audio
 
 if __name__ == "__main__":
     # Converting csv to python object.
-    print("We're are converting the csv table into python object...")
+    print("We're converting the csv table into python object...")
     records_array = []
-    features = 12950
+    features = 3874
 
     for line in range(0, len(data)):
-        song_final_time = int(song_time(data.Path[line])) - int(song_time(data.Path[line])) % 10
-        for i in range(0, song_final_time, 10):
-            records_array.append(Record(data.Accent[line], wav_to_mfcc(divide_audio(data.Path[line], i, i + 10))[0:12950]))
+        song_final_time = int(song_time(data.Path[line])) - int(song_time(data.Path[line])) % 5
+        for i in range(0, song_final_time, 5):
+            records_array.append(Record(data.Accent[line], wav_to_mfcc(divide_audio(data.Path[line], i, i + 5))[0:3874]))
+    print ("Done")
     shuffle(records_array)
     softmax(records_array)
+    
