5a4963bd50f8cdb5d28c4bbe6692d2c76e96193f
yoshago <33255013+yoshago@users.noreply.github.com>
2018-12-09 14:01:54 +0200

    deleted print len of array in mfcc

--- Multinomial_logistic_regression/Mfcc.py
+++ Multinomial_logistic_regression/Mfcc.py
@@ -11,6 +11,4 @@ def wav_to_mfcc(path):
     for i in range(len(mfcc_feat)):
         for j in range(len(mfcc_feat[0])):
             array_mfcc.append(mfcc_feat[i][j])
-    if(len(array_mfcc)<3887):
-        print(len(array_mfcc))
     return array_mfcc
