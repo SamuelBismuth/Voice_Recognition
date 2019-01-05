2285d7edd9c3708a04fde3139b6eb6df90e432a1
yoshago <33255013+yoshago@users.noreply.github.com>
2019-01-06 01:41:10 +0200

    last approach

--- Multinomial_logistic_regression/TryingAmosCode.py
+++ Multinomial_logistic_regression/TryingAmosCode.py
@@ -11,14 +11,14 @@ def next_batch(num, data, labels):
     idx = np.arange(0 , len(data))
     np.random.shuffle(idx)
     idx = idx[:num]
-    data_shuffle = [data[i] for i in idx]
-    labels_shuffle = [labels[i] for i in idx]
+    data_shuffle = [data[ i] for i in idx]
+    labels_shuffle = [labels[ i] for i in idx]
     return np.asarray(data_shuffle), np.asarray(labels_shuffle)
 
 # Network Parameters
 n_hidden_1 = 256  # 1st layer number of neurons
 n_hidden_2 = 256  # 2nd layer number of neurons
-n_input = 1024  # Data input (array size)
+n_input = 7*7*64  #/ Data input (array size)
 n_classes = 5  # Total classes (0-4 languages)
 
 # Store layers weight & bias
@@ -35,10 +35,11 @@ biases = {
 
 
 # Create model
-def multilayer_perceptron(x):
+def multilayer_perceptron(x, keep_prob):
     # Hidden fully connected layer with 256 neurons
     layer_1 = tf.nn.relu(tf.matmul(x, weights['h1']) + biases['b1'])
     # Hidden fully connected layer with 256 neurons
+    layer_1 = tf.nn.dropout(layer_1, keep_prob)
     layer_2 = tf.nn.relu(tf.matmul(layer_1, weights['h2']) + biases['b2'])
     # Output fully connected layer with a neuron for each class
     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
@@ -49,18 +50,18 @@ def test(data):
     x=0
     while(x<len(data)):
         
-        data[x].mfcc = data[x].mfcc.flatten()
-        if(data[x].accent==0):
-            data[x].accent=[1, 0, 0, 0, 0]
-        elif(data[x].accent==1):
-            data[x].accent=[0, 1, 0, 0, 0]
-        elif(data[x].accent==2):
-            data[x].accent=[0, 0, 1, 0, 0]
-        elif(data[x].accent==3):
-            data[x].accent=[0, 0, 0, 1, 0]
-        elif(data[x].accent==4):
-            data[x].accent=[0, 0, 0, 0, 1]
-        if (len(data[x].mfcc)!=299*13):
+        data[x].mfcc = data[x].mfcc.flatten()[:784]
+        """if(data[x].accent==[1, 0, 0, 0, 0]):
+            data[x].accent=0
+        elif(data[x].accent==[0, 1, 0, 0, 0]):
+            data[x].accent=1
+        elif(data[x].accent==[0, 0, 1, 0, 0]):
+            data[x].accent=2
+        elif(data[x].accent==[0, 0, 0, 1, 0]):
+            data[x].accent=3
+        elif(data[x].accent==[0, 0, 0, 0, 1]):
+            data[x].accent=4"""
+        if (len(data[x].mfcc)!=784):
              del data[x] 
         else:
             x=x+1
@@ -73,42 +74,28 @@ def test(data):
 
     print(train_dataset.shape)
 
-    x = tf.placeholder(tf.float32, shape=[None, 299*13])
+    x = tf.placeholder(tf.float32, shape=[None, 784])
     y_ = tf.placeholder(tf.float32, shape=[None, 5])
 
-    W_conv1 = tf.Variable(tf.truncated_normal([10, 10, 1, 32], stddev=0.1))
+    W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
     b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
-    x_image = tf.reshape(x, [-1, 299, 13, 1])  # if we had RGB, we would have 3 channels
+    x_image = tf.reshape(x, [-1, 28, 28, 1])  # if we had RGB, we would have 3 channels
 
     h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
-    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 13, 13, 1], strides=[1, 13, 13, 1], padding='VALID')
+    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    W_conv2 = tf.Variable(tf.truncated_normal([10, 10, 32, 64], stddev=0.1))
+    W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
     b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
-    print(h_pool1)
 
-    #h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
-    #h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 13, 23, 1], strides=[1, 13, 23, 1], padding='SAME')
+    h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1, 1, 1, 1], padding='SAME') + b_conv2)
+    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    h_pool2_flat = tf.reshape(h_pool1, [-1, 1 * 23 * 32])
-    W_fc1 = tf.Variable(tf.truncated_normal([23 * 1 * 32, 1024], stddev=0.1))
-    b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
-
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 
     keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    y_conv = multilayer_perceptron(h_pool2_flat,keep_prob)
 
-    print("hui")
-    W_fc2 = tf.Variable(tf.truncated_normal([1024, 5], stddev=0.1))
-    print("hui2")
-
-    b_fc2 = tf.Variable(tf.constant(0.1, shape=[5]))
-
-    print("hui3")
-
-    y_conv = multilayer_perceptron(h_fc1_drop)
-
+    
 
     print("hui4")
 
@@ -130,10 +117,9 @@ def test(data):
     print("hello")
 
     for i in range(400):
-        batch = next_batch(128, train_dataset, train_labels)
+        batch = next_batch(800, train_dataset, train_labels)
         if i % 100 == 0:
             print("hello2")
-            print(train_labels[0])
             print(batch[0].shape)
             print(batch[1].shape)
             train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
